3c2f5ba70b526081742b455c333315cc8e898929
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-31 20:07:33 +0000

    Return NULL if parent is empty II

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,8 +10,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	right_node = malloc(sizeof(binary_tree_t));
-	if (right_node == NULL || parent == NULL)
+	if (right_node == NULL)
 		return (NULL);
 
 	right_node->n = value;
@@ -23,5 +26,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = right_node;
 
 	parent->right = right_node;
+
 	return (right_node);
 }
