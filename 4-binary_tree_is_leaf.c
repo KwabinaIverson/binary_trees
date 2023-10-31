c54cbdb3d3c2f6538c2f76d8a22849627a5aafc3
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-31 23:09:02 +0000

    Check if node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if node is a leaf in BT
+ * @node: node to check
+ * Return: 1 if node is a leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/* Store value of node we want to find */
+	const binary_tree_t *tmp;
+
+	if (node == NULL)
+		return (0);
+
+	tmp = binary_tree_node(NULL, 98);
+	tmp = node;
+
+	/* Traverse BT looking for node */
+	/* If true, look for children */
+	/* If true, return 0 */
+	binary_tree_is_leaf(node->left);
+	if (node == tmp)
+	{
+		if (node->left != NULL || node->right != NULL)
+			return (0);
+	}
+	binary_tree_is_leaf(node->right);
+
+	return (1);
+}
