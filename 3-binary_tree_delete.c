68f4dd8e8a597e4e1604f7c969224c316a034f2f
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-31 20:56:13 +0000

    delete binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ *  Inorder traversal - use inorder traversal to delete binary tree
+ *  @root: root node of tree
+ *  Return: nothing
+ */
+void inorderTraversal(binary_tree_t *root)
+{
+        if (root == NULL)
+                 return;
+
+        inorderTraversal(root->left);
+        free(root);
+        inorderTraversal(root->right);
+}
+
+/**
+ * binary_tree_delete - deletes a binary tree
+ * @tree: binary tree to delete
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+        inorderTraversal(tree);
+}
