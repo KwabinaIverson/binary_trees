dd3727dd835bdd0ad18855573e9376d80db91ca5
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-01 01:11:41 +0000

    Measures the height of a binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * size_t binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height og tree on success, 0 on failure
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return 0;
+
+	size_t height_l;
+	size_t height_r;
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
