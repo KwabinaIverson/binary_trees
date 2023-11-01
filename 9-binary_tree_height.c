36f124669325df379fc09357a1f744616c0f2802
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-01 01:16:08 +0000

    Add parentheses to return statement

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * size_t binary_tree_height - measures the height of a binary tree
+ * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  * Return: height og tree on success, 0 on failure
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	size_t height_l;
 	size_t height_r;
