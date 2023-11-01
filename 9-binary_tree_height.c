137ad526b1a88d22e6100c8eca4328b82f44b486
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-01 02:53:04 +0000

    remove whitespace

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-        size_t height_l, height_r;
+	size_t height_l, height_r;
 
 	if (tree == NULL)
 		return (0);
@@ -16,5 +16,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-        return (height_l > height_r ? height_l : height_r);
+	return (height_l > height_r ? height_l : height_r);
 }
