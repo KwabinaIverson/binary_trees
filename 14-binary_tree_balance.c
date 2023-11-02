bcf2b1b94cbfa55e5360282959296f122608b0a3
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-02 02:50:54 +0000

    measures the balance factor of a binary tree III

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,10 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
-	return (height_l - height_r);
+	return (height_l > height_r ? height_l : height_r);
 }
 
 /**
@@ -26,12 +26,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t ans;
+	int height_l, height_r;
 
 	if (tree == NULL)
 		return (0);
 
-	ans = binary_tree_height(tree);
+	height_l = (int)binary_tree_height(tree->left);
+	height_r = (int)binary_tree_height(tree->right);
 
-	return ((int)ans);
+	return (height_l - height_r);
 }
