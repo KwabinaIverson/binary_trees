15884b84b231b74cfcc5437c203070639335b667
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-11-02 00:57:39 +0000

    checks if a binary tree is full II

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
+	if ((tree->left != NULL && tree->right == NULL) ||
+		(tree->left == NULL && tree->right != NULL))
 		count++;
 
 	/* count left/right sub-tree */
